ec455c468a89c9384dbcb71cf827a40f8cc8521e
antik1801 <antik.edu@gmail.com>
2023-03-24 16:04:24 +0600

    video 2

---
+++ understanding-react-core-concepts-with-react-app/src/components/DeviceDetails/DeviceDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeviceDetails = (props) => {
+    return (
+        <div>
+            <h3>Information about the phone: Price={props.price}</h3>
+        </div>
+    );
+};
+
+export default DeviceDetails;
