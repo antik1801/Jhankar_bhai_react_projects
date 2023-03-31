e11a91e25f84f01d6322299e87fef32a7fbc1bc3
antik1801 <antik.edu@gmail.com>
2023-03-31 22:55:10 +0600

    main commit

---
+++ fifa/README.md
@@ -0,0 +1,13 @@
+# Netlify Link: 
+```
+https://classy-bublanina-4ef369.netlify.app/
+```
+# Github Link:
+```
+https://github.com/antik1801/Jhankar_bhai_react_projects/tree/main/fifa
+```
+
+# Screen-shots: 
+<img src="./1.PNG" style="height: 300px; width: 100%">
+<img src="./2.PNG" style="height: 300px; width: 100%">
+<!-- <img src="./3.PNG" style="height: 300px; width: 100%"> -->
--- fifa/src/components/MainContainer/MainContainer.jsx
+++ fifa/src/components/MainContainer/MainContainer.jsx
@@ -1,47 +1,58 @@
-import React from 'react';
-import { useEffect, useState } from 'react'
-import { addToDbPlayers, getPlayersInfo } from '../../utilities/football';
-import Home from '../Home/Home';
-import SideCart from '../SideCart/SideCart';
-import "./MainContainer.css"
+import React from "react";
+import { useEffect, useState } from "react";
+import { addToDbPlayers, getPlayersInfo } from "../../utilities/football";
+import Home from "../Home/Home";
+import SideCart from "../SideCart/SideCart";
+import "./MainContainer.css";
 
 const MainContainer = () => {
-    const [players,setPlayers] = useState([]);
-    const [selectedPlayers, setSelectedPlayers] = useState([])
-    const handleBuyNow = (player) =>{
-        const newPlayer = [...selectedPlayers, player]
-        setSelectedPlayers(newPlayer);
-        addToDbPlayers(player._id)
+  const [players, setPlayers] = useState([]);
+  const [selectedPlayers, setSelectedPlayers] = useState([]);
+  const handleBuyNow = (players) => {
+    for (const player of selectedPlayers) {
+      if (players._id === player._id) {
+        return;
       }
-  useEffect(()=>{
+    }
+    const newPlayer = [...selectedPlayers, players];
+    setSelectedPlayers(newPlayer);
+    addToDbPlayers(players._id);
+  };
+  useEffect(() => {
     fetch("player.json")
-    .then(res => res.json())
-    .then(data => setPlayers(data))
-  },[])
-  
-  useEffect(()=>{
+      .then((res) => res.json())
+      .then((data) => setPlayers(data));
+  }, []);
+
+  useEffect(() => {
     // console.log(players);
-    const storedPlayers = getPlayersInfo ();
+    const storedPlayers = getPlayersInfo();
     const addedPlayers = [];
     // step 1> get ID
     // console.log(storedPlayers);
     for (const id in storedPlayers) {
-        // console.log(id);
-        const savePlayers = players.find(player =>player._id  === id);
-        addedPlayers.push(savePlayers);
+      // console.log(id);
+      const savePlayers = players.find((player) => player._id === id);
+      addedPlayers.push(savePlayers);
     }
     setSelectedPlayers(addedPlayers);
-  },[players])
-    return (
-        <div className='home-container'>
-            <div className='row row-cols-1 row-cols-md-3 g-4'>{
-                players.map(player=> <Home player={player} key={player._id} handleBuyNow={handleBuyNow}></Home>)
-            }</div>
-            <div className='side-cart'>
-            <SideCart selectedPlayers={selectedPlayers}></SideCart>
-            </div>
-        </div>
-    );
+  }, [players]);
+  return (
+    <div className="home-container">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {players.map((player) => (
+          <Home
+            player={player}
+            key={player._id}
+            handleBuyNow={handleBuyNow}
+          ></Home>
+        ))}
+      </div>
+      <div className="side-cart">
+        <SideCart selectedPlayers={selectedPlayers}></SideCart>
+      </div>
+    </div>
+  );
 };
 
 export default MainContainer;
