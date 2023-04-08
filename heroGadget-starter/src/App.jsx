18b3618025542c9fe86fd15c92472ed9162c411e
antik1801 <antik.edu@gmail.com>
2023-04-09 02:44:45 +0600

    seen 1hour 08 second

--- heroGadget-starter/src/App.jsx
+++ heroGadget-starter/src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -5,7 +6,9 @@ import Home from "./components/Home";
 const App = () => {
   return(<>
     <Header></Header>
-    <Home></Home>
+    <div className="min-h-[calc(100vh-137px)]">
+    <Outlet></Outlet>
+    </div>
     <Footer></Footer>
   </>);
 };
--- heroGadget-starter/src/main.jsx
+++ heroGadget-starter/src/main.jsx
@@ -4,16 +4,31 @@ import './index.css'
 import App from './App'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import About from './components/About'
+import Home from './components/Home'
+import ErrorPage from './components/ErrorPage'
+import Shop from './components/Shop'
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App></App>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+            },
+            {
+                path: "/shop",
+                element: <Shop></Shop>,
+                loader: ()=> fetch('https://raw.githubusercontent.com/antik1801/Jhankar_bhai_react_projects/main/heroGadget-starter/public/products.json'),
+            },
+            {
+                path: "/about",
+                element: <About />,
+            }
+        ]
     },
-    {
-        path: '/about',
-        element: <About></About>,
-    }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}></RouterProvider>)
