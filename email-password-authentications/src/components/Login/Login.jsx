d426e2bb1844362555d539a9c0a01c64a3a3cbe9
antik1801 <antik.edu@gmail.com>
2023-04-18 09:01:58 +0600

    login page

--- email-password-authentications/src/components/Login/Login.jsx
+++ email-password-authentications/src/components/Login/Login.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>This is login</h2>
+    const [error,setError] = useState("");
+    const [success, setSuccess] = useState("");
+    const handleLogin = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password);
+
+        if (!/(?=.*?[A-Z].*[A-Z])/.test(password)) {
+            setError('Please add two uppercase in your password')            
+        }
+        else if (!/(?=.*?[#?!@$%^&*-])/.test(password))
+        {
+            setError('Please add at least one special charector');
+            return ;
+        }
+
+    }
+  return (
+    <div>
+      <h2>Please Login</h2>
+      <p className="text-danger">{error}</p>
+      <Form onSubmit={handleLogin}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email"  name="email" required/>
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" name="password"/>
+        </Form.Group>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" required/>
+          <label className="form-check-label">
+           Accept terms and conditions
+          </label>
         </div>
-    );
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Login;
--- email-password-authentications/src/components/Register/Register.jsx
+++ email-password-authentications/src/components/Register/Register.jsx
@@ -1,66 +1,101 @@
-import React, { useState } from 'react';
-import "./Register.css"
-import { app } from '../Firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth"; 
-import { toast } from 'react-hot-toast';
+import React, { useState } from "react";
+import "./Register.css";
+import { app } from "../Firebase/firebase.config";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { toast } from "react-hot-toast";
 
 const auth = getAuth(app);
 
 const Register = () => {
-    const [mail,setMail] = useState('');
-    const [pass,setPass] = useState('');
-    const [control,setControl] = useState(false);
-    const [error,setError] = useState('');
-    // const handlePasswordChange = (event) =>{
-    //     // console.log(event.target.value);
-    //     setPass(event.target.value);
-    // }
-    // const handleEmailChange = (event) =>{
-    //     // console.log(event.target.value);
-    //     setMail(event.target.value);
-    // }
-    const handleSubmit = (event) =>{
-        
-        event.preventDefault();
-        const email = (event.target.email.value);
-        const password = (event.target.password.value);
-        console.log(email,password);
-        // create user in firebase
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(result=>{
-            const user = result.user;
-            console.log(user);
-            setError('');
-        })
-        .catch(error => {
-            setError(error.message);
-            toast.error(error.message);
-        })
+  const [mail, setMail] = useState("");
+  const [pass, setPass] = useState("");
+  const [control, setControl] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSucces] = useState("");
+
+  // const handlePasswordChange = (event) =>{
+  //     // console.log(event.target.value);
+  //     setPass(event.target.value);
+  // }
+  // const handleEmailChange = (event) =>{
+  //     // console.log(event.target.value);
+  //     setMail(event.target.value);
+  // }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setError('');
+    setSucces("");
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+    if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(pass)) {
+         setError('Password must contain 8 charector 1 upper 1 lower and 1 special charector');
+         return;
     }
-    return (
+    // create user in firebase
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setError("");
+        event.target.reset();
+        setSucces("user has created successfully");
+        toast.success("user has created successfully");
+      })
+      .catch((error) => {
+        setError(error.message);
+        toast.error(error.message);
+      });
+
+  };
+  return (
+    <div>
+      <h2>Please Register if you are new</h2>
+      <p className="text-danger">{error}</p>
+      <p className="text-success">{success}</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter your email address here"
+          required
+        />
+        <br />
         <div>
-            <h2>Please Register if you are new</h2>
-            <p className='text-danger'>{error}</p>
-            <form onSubmit={handleSubmit}>
-                <input type="email" name="email" id="email" placeholder='Enter your email address here'  required/>
-                <br />
-                <div> 
-                {   control ?
-                    <div className='btn-pass'>
-                    <input type="text" name="password" id="password" placeholder='Enter your password' required/>
-                    <button className='btn-show' onClick={()=>setControl(!control)}>Hide</button> 
-                    </div>:
-                    <div className='btn-pass'>
-                    <input type="password" name="password" id="password" placeholder='Enter your password'  required/> 
-                    <button className='btn-show' onClick={()=>setControl(!control)}>See</button>
-                    </div>
-                }
-                </div>
-                <br />
-                <input type="submit" value="Register"/>
-            </form>
+          {control ? (
+            <div className="btn-pass">
+              <input
+                type="text"
+                name="password"
+                id="password"
+                placeholder="Enter your password"
+                required
+              />
+              <button className="btn-show" onClick={() => setControl(!control)}>
+                Hide
+              </button>
+            </div>
+          ) : (
+            <div className="btn-pass">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter your password"
+                required
+              />
+              <button className="btn-show" onClick={() => setControl(!control)}>
+                See
+              </button>
+            </div>
+          )}
         </div>
-    );
+        <br />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
 };
 
 export default Register;
