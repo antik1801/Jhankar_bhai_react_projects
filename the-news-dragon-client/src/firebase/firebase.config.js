547ac0ef9327af15e8ad2551f2ae0b406a482689
antik1801 <antik.edu@gmail.com>
2023-04-23 21:43:08 +0600

    the dragon news resources

---
+++ the-news-dragon-client/src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_4DCYqtyy2ACRzjkAa-7C8vjLGRqlRhQ",
+    authDomain: "the-news-dragon-ded83.firebaseapp.com",
+    projectId: "the-news-dragon-ded83",
+    storageBucket: "the-news-dragon-ded83.appspot.com",
+    messagingSenderId: "885697906984",
+    appId: "1:885697906984:web:92f30dda65776feb21e81d"
+};
+
+
+export const app = initializeApp(firebaseConfig);
