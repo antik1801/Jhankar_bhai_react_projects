8ba3f70844da4e03a0111b712943c2cb7a0e82af
antik1801 <antik.edu@gmail.com>
2023-04-28 21:07:12 +0600

    someone is heading behind

--- the-news-dragon-client/src/pages/Home/NewsCart/NewsCart.jsx
+++ the-news-dragon-client/src/pages/Home/NewsCart/NewsCart.jsx
@@ -2,11 +2,13 @@ import moment from "moment";
 import React from "react";
 import { Image } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
-import { FaRegBookmark, FaShareAlt } from "react-icons/fa";
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt , FaStar} from "react-icons/fa";
+import Rating from "react-rating";
 import { Link } from "react-router-dom";
 
+
 const NewsCart = ({ news }) => {
-  const { _id, title, details, image_url, author } = news;
+  const { _id, title, details, image_url, author, total_view, rating } = news;
   return (
     <div>
       <Card className="mb-4">
@@ -35,7 +37,25 @@ const NewsCart = ({ news }) => {
             )}
           </Card.Text>
         </Card.Body>
-        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        <Card.Footer className="text-muted d-flex">
+          <div className="flex-grow-1">
+              <Rating
+              readonly
+              placeholderRating={rating.number}
+              emptySymbol={<FaRegStar></FaRegStar>}
+              placeholderSymbol={<FaStar></FaStar>}
+              fullSymbol={<FaStar></FaStar> }
+              className="text-warning"
+              title="Ratings"
+              >
+              </Rating>
+           <span> {rating?.number} </span>
+          </div>
+            <div>
+                <FaEye></FaEye>
+                {total_view}
+            </div>
+        </Card.Footer>
       </Card>
     </div>
   );
