fcfaae8182a1f9d41633ce7fa82e5617f42011a2
antik1801 <antik.edu@gmail.com>
2023-05-06 19:53:50 +0600

    main

---
+++ users-management-clientsite/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+  const handleAddUser = event =>{
+    event.preventDefault()
+    const form= event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    // console.log(name,email);
+    const user = { name, email}
+    console.log(user)
+
+  }
+  const [users, setUsers] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[])
+  return (
+    <>
+      <h1>Users Managements System</h1>
+      <p>Numbers of users: {users.length}</p>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' required/>
+        <br />
+        <input type="email" name='email' required/>
+        <br />
+        <input type="submit" value="add user" />
+      </form>
+      {
+        users.map(user => <p key={user.id}>{user.id} : {user.name} : {user.email}</p> )
+      }
+    </>
+  )
+}
+
+export default App
