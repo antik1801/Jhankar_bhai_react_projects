6c4c6317554064d7773f70a1d384796614737917
antik1801 <antik.edu@gmail.com>
2023-05-07 19:44:11 +0600

    main

---
+++ simple-crud-client/src/components/Update.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+const Update = () => {
+  const loadedUser = useLoaderData();
+
+  const handleUpdate = event =>{
+    event.preventDefault()
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    console.log(name,email)
+  }
+  return (
+    <div>
+      <h3>Update information of {loadedUser.name}</h3>
+      <form onSubmit={handleUpdate}>
+        <input type="text" name="name" defaultValue={loadedUser?.name} />
+        <br />
+        <input type="email" name="email" defaultValue={loadedUser?.email} />
+        <br />
+        <input type="submit" value="Update" />
+      </form>
+    </div>
+  );
+};
+
+export default Update;
---
+++ simple-crud-client/src/components/Users.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
+
+const Users = () => {
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+  const handleDeleteUser = (id) => {
+    console.log(id);
+    fetch(`http://localhost:5000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          alert("Deleted successfully");
+          const remaining = users.filter((user) => user._id != id);
+          setUsers(remaining);
+        }
+      });
+  };
+  return (
+    <div>
+      <Link to={"/"}>
+        <button>home</button>
+      </Link>
+      <h2>{loadedUsers.length}</h2>
+      <div>
+        {loadedUsers.map((user) => (
+          <p key={user._id}>
+            {user.name} : {user.email} {user._id}{" "}
+            <Link to={`/update/${user._id}`}>
+              <button>Update</button>
+            </Link>
+            <button onClick={() => handleDeleteUser(user._id)}>X</button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Users;
