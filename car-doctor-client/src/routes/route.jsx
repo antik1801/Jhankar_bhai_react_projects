09830e31f32d68e24dfd0139aaf40d32e0dbb642
antik1801 <antik.edu@gmail.com>
2023-05-10 13:48:09 +0600

    main

---
+++ car-doctor-client/src/routes/route.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+const route = createBrowserRouter([
+    {
+        path: "/",
+        element: <App></App>
+    }
+])
+
+export default route;
