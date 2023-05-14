310048895f9180e4d5a97d16b2338cbad8a8580c
antik1801 <antik.edu@gmail.com>
2023-05-14 21:11:34 +0600

    main

--- car-doctor-client/src/Pages/Bookings/Bookings.jsx
+++ car-doctor-client/src/Pages/Bookings/Bookings.jsx
@@ -50,7 +50,12 @@ const Bookings = () => {
   };
   const url = `http://localhost:5000/bookings?email=${user?.email}`;
   useEffect(() => {
-    fetch(url)
+    fetch(url,{
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('car-access-token')}`,
+      }
+    })
       .then((res) => res.json())
       .then((data) => setBookings(data));
   }, [render]);
