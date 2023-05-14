737f2b1391e2c5b09f5f8a7f056134b34ff18d8c
antik1801 <antik.edu@gmail.com>
2023-05-14 07:22:49 +0600

    delete done

--- book-manager-server/index.js
+++ book-manager-server/index.js
@@ -63,6 +63,12 @@ async function run() {
       const result = await booksCollection.updateOne(filter,updateDoc)
       res.send(result)
     })
+    app.delete('/books/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await booksCollection.deleteOne(query)
+      res.send(result)
+    })
 
   
 
