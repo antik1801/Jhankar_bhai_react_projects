ba926d79edad55d8e5d11af9b18586e432f56599
antik1801 <antik.edu@gmail.com>
2023-05-15 19:56:21 +0600

    main

--- ema-john-server/index.js
+++ ema-john-server/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
