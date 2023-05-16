0813dca57736c9b374ee985e350ec4900156a326
antik1801 <antik.edu@gmail.com>
2023-05-17 01:59:54 +0600

    server

--- volenteer-network-server/index.js
+++ volenteer-network-server/index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zycuvps.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,11 +25,42 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const volenteerCollection = client.db('volenteerDB').collection('volenteer')
-
+    const serviceCollection = client.db('volenteerDB').collection('services')
+    //Read data from the category
+    app.get('/allCategories', async(req,res)=>{
+        const result = await volenteerCollection.find().toArray()
+        res.send(result)
+    })
+    // Read all services
+    app.get('/services', async(req,res)=>{
+        const result = await serviceCollection.find().toArray()
+        res.send(result)
+    })
+    // Add data to the service
+    app.post('/services', async(req,res)=>{
+        const service = req.body
+        console.log(service)
+        const result = await serviceCollection.insertOne(service)
+        res.send(result)
+    })
+    //Specific data update
+    app.patch('/services/:id', async(req,res)=>{
+        const id = req.params.id;
+        const updateinfo = req.body;
+        console.log(id);
+        const filter = {_id:new ObjectId(id)}
+        const updateService = {
+            $set:{
+                name: updateinfo.name,
+                address: updateinfo.address,
+                photo: updateinfo.photo,
+                status: updateinfo.status,
+            }
+        }
+        const result = await serviceCollection.updateOne(filter,updateService)
+        res.send(result)
+    })
     
-
-
-
 
 
     // Send a ping to confirm a successful connection
