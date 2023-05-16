7a69f5152c0810150ce8d6267622d48882460ed1
antik1801 <antik.edu@gmail.com>
2023-05-17 02:19:54 +0600

    server

--- volenteer-network-server/index.js
+++ volenteer-network-server/index.js
@@ -1,14 +1,14 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-
+const jwt = require('jsonwebtoken')
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zycuvps.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
