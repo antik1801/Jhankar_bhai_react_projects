73b9b0944d7c17890845b71c27c3575ebf1cdba2
antik1801 <antik.edu@gmail.com>
2023-05-19 19:09:38 +0600

    main

--- job-portal-cliend-test/src/pages/AddJob/AddJob.jsx
+++ job-portal-cliend-test/src/pages/AddJob/AddJob.jsx
@@ -21,6 +21,17 @@ const AddJob = () => {
   const onSubmit = (data) =>{
     data.skills = selectedOption;  
     console.log(data)
+    fetch('http://localhost:5000/postjob',{
+      method: 'POST',
+      headers: {
+        'content-type' : 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
   };
 
   const options = [
