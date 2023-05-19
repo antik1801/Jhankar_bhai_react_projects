e6cfcfe26eaef574480234be619e3c454bf9bcb5
antik1801 <antik.edu@gmail.com>
2023-05-19 20:06:37 +0600

    job portal 1

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -15,38 +15,53 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    
-
-
-
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const jobCollection = client.db('jobDB').collection('jobs')
+        // Add a Job
+        app.post('/postjob', async (req, res) => {
+            try {
+                const body = req.body;
+                const result = await jobCollection.insertOne(body);
+                res.send(result);
+            } catch (error) {
+                res.send(error.message)
+            }
+        })
+        // Find all jobs 
+        app.get('/allJobs', async(req,res)=>{
+            try {
+                const result = await jobCollection.find().toArray();
+                res.send(result)
+            } catch (error) {
+                res.send(error.message)
+            }
+        })
+        //
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Job Portal is online')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`JOB PORTAL is running in server port ${port}`)
 })
