8f84d2692f6bc71682eca8d3baefcfd3fda3484f
antik1801 <antik.edu@gmail.com>
2023-05-23 01:29:22 +0600

    main

--- bistro-boss-client/src/App.jsx
+++ bistro-boss-client/src/App.jsx
@@ -1,15 +1,15 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      
+      <h1 className="">Hello world!</h1>
+      <button className="btn btn-outline btn-info">Info</button>
+      <button className="btn btn-outline btn-success">Success</button>
+      <button className="btn btn-outline btn-warning">Warning</button>
+      <button className="btn btn-outline btn-error">Error</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ bistro-boss-client/src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>I am From Home</h1>
+        </div>
+    );
+};
+
+export default Home;
