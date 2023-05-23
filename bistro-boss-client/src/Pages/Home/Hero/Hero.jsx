e6e0935e1e9c27d5db460b3b9ae86a1c0732d2ed
antik1801 <antik.edu@gmail.com>
2023-05-23 20:13:09 +0600

    main

---
+++ bistro-boss-client/src/Pages/Home/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import chef from "../../../assets/home/chef-service.jpg"
+
+const Hero = () => {
+    return (
+        <div className='relative'>
+            <img src={chef} alt="" />
+            <div className='w-4/6 flex justify-center items-center bg-white text-black text-center flex-col absolute top-28 left-52 p-10'>
+            <p className='text-3xl mb-5'>Bistro Boss</p>
+            <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eligendi nam, architecto beatae nostrum quo et facere laborum amet rem dignissimos ipsam, incidunt veniam sequi dolores aliquam dolor iste distinctio!</p>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
--- bistro-boss-client/src/Pages/Home/Home.jsx
+++ bistro-boss-client/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Bannar/Banner";
 import Category from "./Category/Category";
+import Hero from "./Hero/Hero";
 
 const Home = () => {
     return (
         <div className="space-y-20">
             <Banner></Banner>
             <Category></Category>
+            <Hero></Hero>
         </div>
     );
 };
