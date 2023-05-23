d496399dd350852795ee9dbd8e6e9208c27ff067
antik1801 <antik.edu@gmail.com>
2023-05-23 20:22:30 +0600

    main

--- bistro-boss-client/src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ bistro-boss-client/src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle';
 
 const PopularMenu = () => {
+    const [menu,setMenu] = useState([])
+    useEffect(()=>{
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data => setMenu(data))
+    },[])
     return (
         <section>
             <SectionTitle heading="From Our Menu" subheading="Check it Out"></SectionTitle>
