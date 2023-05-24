556e7a13096d67ef6138f82455911b1ad16c4e3d
antik1801 <antik.edu@gmail.com>
2023-05-24 20:55:16 +0600

    main

--- bistro-boss-client/src/Pages/Order/Order/Order.jsx
+++ bistro-boss-client/src/Pages/Order/Order/Order.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import orderBG from "../../../assets/shop/order.jpg";
 import Cover from "../../Shared/Cover/Cover";
+import Tab from "../../../components/Tab";
 
 const Order = () => {
+    const [active,setActive] = useState("tab1")
+    const handleActive = active =>{
+        setActive(active);
+    }
   return (
     <div>
       <Cover title={"Order Cover"} img={orderBG}></Cover>
-      <div className="tabs">
-        <a className="tab tab-bordered">Tab 1</a>
-        <a className={`tab tab-bordered tab-active`}>Tab 2</a>
-        <a className="tab tab-bordered">Tab 3</a>
+      <div className="flex justify-center">
+        <Tab active={active} handleActive={handleActive}></Tab>
       </div>
     </div>
   );
--- bistro-boss-client/src/components/Tab.jsx
+++ bistro-boss-client/src/components/Tab.jsx
@@ -1,1 +1,13 @@
-rsc
+import React from "react";
+
+const Tab = ({active,handleActive}) => {
+  return (
+    <div className="tabs mb-20">
+      <a className={`tab tab-bordered ${active === 'tab1' ? 'tab-active' : 'tab-lifted'}`} onClick={()=>handleActive("tab1")}>Tab 1</a>
+      <a className={`tab tab-bordered ${active === 'tab2' ? 'tab-active' : 'tab-lifted'}`} onClick={()=>handleActive("tab2")}>Tab 2</a>
+      <a className={`tab tab-bordered ${active === 'tab3' ? 'tab-active' : 'tab-lifted'}`} onClick={()=>handleActive("tab3")}>Tab 3</a>
+    </div>
+  );
+};
+
+export default Tab;
