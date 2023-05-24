472ac1751705578b2cf974aa569b6d457e8367da
antik1801 <antik.edu@gmail.com>
2023-05-24 20:22:27 +0600

    main

---
+++ bistro-boss-client/src/Pages/Order/Order/Order.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import orderBG from '../../../assets/shop/order.jpg'
+import Cover from '../../Shared/Cover/Cover';
+
+const Order = () => {
+    return (
+        <div>
+            <Cover title={"Order Cover"} img={orderBG}></Cover>
+        </div>
+    );
+};
+
+export default Order;
--- bistro-boss-client/src/Pages/Shared/Cover/Cover.jsx
+++ bistro-boss-client/src/Pages/Shared/Cover/Cover.jsx
@@ -13,7 +13,7 @@ const Cover = ({ img, title }) => {
       <div className="hero h-[500px]">
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md bg-black bg-opacity-30 p-10">
+          <div className="max-w-md bg-black bg-opacity-40 p-10">
             <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
             <p className="mb-5">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
