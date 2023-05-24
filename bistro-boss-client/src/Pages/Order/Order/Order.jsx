87b832e3a59a454c29ec1ca744e90b34fa70218c
antik1801 <antik.edu@gmail.com>
2023-05-24 20:26:31 +0600

    main

--- bistro-boss-client/src/Pages/Order/Order/Order.jsx
+++ bistro-boss-client/src/Pages/Order/Order/Order.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import orderBG from '../../../assets/shop/order.jpg'
-import Cover from '../../Shared/Cover/Cover';
+import React from "react";
+import orderBG from "../../../assets/shop/order.jpg";
+import Cover from "../../Shared/Cover/Cover";
 
 const Order = () => {
-    return (
-        <div>
-            <Cover title={"Order Cover"} img={orderBG}></Cover>
-        </div>
-    );
+  return (
+    <div>
+      <Cover title={"Order Cover"} img={orderBG}></Cover>
+      <div className="tabs">
+        <a className="tab tab-bordered">Tab 1</a>
+        <a className={`tab tab-bordered tab-active`}>Tab 2</a>
+        <a className="tab tab-bordered">Tab 3</a>
+      </div>
+    </div>
+  );
 };
 
 export default Order;
