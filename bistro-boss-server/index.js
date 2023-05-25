03ef3c18efd76ba0d5ff10090188c329196f449f
antik1801 <antik.edu@gmail.com>
2023-05-25 12:39:36 +0600

    main

--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -1,46 +1,48 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
 
-/*
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.zycuvps.mongodb.net/?retryWrites=true&w=majority";
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env
+    .DB_PASS}@cluster0.zycuvps.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
-*/
 
 
 
-app.get('/', (req,res)=>{
+
+app.get('/', (req, res) => {
     res.send(`Boss is sitting ${port}`);
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Bistro Boss is sitting on the port', port);
 })
