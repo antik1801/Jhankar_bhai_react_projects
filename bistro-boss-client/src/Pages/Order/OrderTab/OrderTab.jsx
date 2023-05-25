30aa263cc2ae1b6c77d5c7a69f0a3421bc078f89
antik1801 <antik.edu@gmail.com>
2023-05-25 17:09:11 +0600

    main

--- bistro-boss-client/src/Pages/Order/OrderTab/OrderTab.jsx
+++ bistro-boss-client/src/Pages/Order/OrderTab/OrderTab.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import FoodCard from "../../../components/FoodCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
-const OrderTab = ({items,img}) => {
+const OrderTab = ({ items, img }) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
+
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 space-y-6">
-      {items.map((item) => (
-        <FoodCard key={item._id} item={item} img={img}></FoodCard>
-      ))}
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          {items.map((item) => (
+            <FoodCard key={item._id} item={item} img={img}></FoodCard>
+          ))}
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
