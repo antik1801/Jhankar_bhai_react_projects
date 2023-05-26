2ee9355604f49d7d31ab982b6adb2688a3418afc
antik1801 <antik.edu@gmail.com>
2023-05-26 17:57:47 +0600

    main

--- bistro-boss-client/src/firebase/firebase.config.js
+++ bistro-boss-client/src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZbi-beu_tNr2rIqkeaOpmoEJ8yzHNBGo",
-  authDomain: "bistro-boss-363c7.firebaseapp.com",
-  projectId: "bistro-boss-363c7",
-  storageBucket: "bistro-boss-363c7.appspot.com",
-  messagingSenderId: "224907430865",
-  appId: "1:224907430865:web:9c9c5f9b2752057d32eef0"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
