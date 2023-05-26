148c87800b277ad8898cd6a7db078ce8e17fbfb5
antik1801 <antik.edu@gmail.com>
2023-05-26 18:40:03 +0600

    main

--- bistro-boss-client/src/ContextProviders/AuthProvider.jsx
+++ bistro-boss-client/src/ContextProviders/AuthProvider.jsx
@@ -1,10 +1,54 @@
-import React, { createContext, useState } from 'react';
-export const AuthContext = createContext(null)
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, updateEmail, updateProfile } from "firebase/auth";
+import { app } from '../firebase/firebase.config';
 
+
+export const AuthContext = createContext(null)
+const auth = getAuth(app);
 const AuthProvider = ({children}) => {
-    const [user,setUser] = useState(null)
-    
-    const authInfo = {}
+    const [user,setUser] = useState(null);
+    const [loading,setLoading] = useState(true);
+
+    const createUser = (email,password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+    const login = (email,password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+    const setPictureAndName = (user, userName, userImage) =>{
+        return updateProfile(user, {
+            displayName: userName,
+            photoURL: userImage,
+        })
+    }
+    const emailUpdate = (user,newEmail) =>{
+        return updateEmail(user,newEmail);
+    }
+    const varifyMail = user =>{
+        return sendEmailVerification(user)
+    }
+    useEffect(()=>{
+        const unsubscribed = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            setLoading(false);
+            console.log('Current User', currentUser);
+        })
+        return ()=>{ 
+           return unsubscribed();
+        } 
+    },[])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        login,
+        setPictureAndName,
+        emailUpdate,
+        varifyMail,
+    }
     return (
        <AuthContext.Provider value={authInfo}>
         {children}
--- bistro-boss-client/src/main.jsx
+++ bistro-boss-client/src/main.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routers/router'
-import '@smastrom/react-rating/style.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routers/router";
+import "@smastrom/react-rating/style.css";
+import AuthProvider from "./ContextProviders/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  
-    <div className='max-w-screen-xl mx-auto'>
-    <RouterProvider router={router}></RouterProvider>
+    <div className="max-w-screen-xl mx-auto">
+      <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthProvider>
     </div>
-
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
