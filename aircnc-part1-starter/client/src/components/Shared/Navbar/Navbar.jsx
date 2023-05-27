6afc861671e0fa5b2e300f70ea53c7f26de0f467
antik1801 <antik.edu@gmail.com>
2023-05-27 23:09:31 +0600

    main

--- aircnc-part1-starter/client/src/components/Shared/Navbar/Navbar.jsx
+++ aircnc-part1-starter/client/src/components/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+import Container from "../Container/Container";
+import Logo from "./Logo";
 
 const Navbar = () => {
-    return (
-        <div className='fixed w-full bg-white z-10 shadow-sm'>
-            <div className='py-4 border-b-[1px]'>
-                
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+            <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+                <Logo></Logo>
+                <div>Search</div>
+                <div>Menu</div>
             </div>
-        </div>
-    );
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
