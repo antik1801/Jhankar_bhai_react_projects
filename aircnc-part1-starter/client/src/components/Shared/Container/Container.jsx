069bdaf759648c130be06798eda44849d1a7d719
antik1801 <antik.edu@gmail.com>
2023-05-27 23:03:17 +0600

    main

--- aircnc-part1-starter/client/src/components/Shared/Container/Container.jsx
+++ aircnc-part1-starter/client/src/components/Shared/Container/Container.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Container = () => {
+const Container = ({children}) => {
     return (
-        <div>
-            
+        <div className='max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4'>
+            {children}
         </div>
     );
 };
