f41aa87a08bf6b6d592e58b3312f08431d3c4b31
antik1801 <antik.edu@gmail.com>
2023-05-28 12:35:39 +0600

    main

--- aircnc-part1-starter/client/src/components/Rooms/Rooms.jsx
+++ aircnc-part1-starter/client/src/components/Rooms/Rooms.jsx
@@ -27,7 +27,7 @@ const Rooms = () => {
         <Container>
             <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
                 {
-                    rooms.map(room=> <Card></Card> )
+                    rooms.map((room,index)=> <Card key={index} room={room}></Card> )
                 }
             </div>
         </Container>
