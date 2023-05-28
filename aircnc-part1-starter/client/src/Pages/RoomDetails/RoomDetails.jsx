09d9420277eeea9e31d646e42215e42db646af50
antik1801 <antik.edu@gmail.com>
2023-05-28 16:21:51 +0600

    main

--- aircnc-part1-starter/client/src/Pages/RoomDetails/RoomDetails.jsx
+++ aircnc-part1-starter/client/src/Pages/RoomDetails/RoomDetails.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import Container from '../../components/Shared/Container/Container';
-import Header from '../../components/Rooms/Header';
-import RoomInfo from '../../components/Rooms/RoomInfo';
+import React from "react";
+import Container from "../../components/Shared/Container/Container";
+import Header from "../../components/Rooms/Header";
+import RoomInfo from "../../components/Rooms/RoomInfo";
+import RoomReseration from "../../components/Rooms/RoomReseration";
 
 const RoomDetails = () => {
-    return (
-       <Container >
-        <div className="max-w-screen-lg mx-auto">
-            <div className="flex flex-col gap-6">
-                <Header></Header>
-                <div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6'>
-                    <RoomInfo></RoomInfo>
-                <div>Reservation</div>
-                </div>
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <Header></Header>
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <RoomInfo></RoomInfo>
+            <div className="mb-10 md:col-span-3 order-1 md:order-last">
+            <RoomReseration></RoomReseration>
             </div>
+          </div>
         </div>
-       </Container>
-    );
+      </div>
+    </Container>
+  );
 };
 
 export default RoomDetails;
