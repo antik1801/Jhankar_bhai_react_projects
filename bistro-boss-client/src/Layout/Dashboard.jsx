48c380fae33a549d821e488d05986025ea0130ee
antik1801 <antik.edu@gmail.com>
2023-05-29 19:40:59 +0600

    client

--- bistro-boss-client/src/Layout/Dashboard.jsx
+++ bistro-boss-client/src/Layout/Dashboard.jsx
@@ -1,14 +1,21 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
-import { FaShoppingCart } from "react-icons/fa";
+import React, { useEffect } from "react";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome } from "react-icons/fa";
 
 const Dashboard = () => {
+    const location = useLocation();
+    useEffect(()=>{
+        document.title = `Bistro ${location.pathname.replace("/", "- ")}`;
+        if (location.state) {
+            document.title = location.state;
+          }
+    },[location])
   return (
     <div>
-      <div className="drawer drawer-mobile">
+      <div className="drawer drawer-mobile ">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center">
-            <Outlet></Outlet>
+          <Outlet></Outlet>
           <label
             htmlFor="my-drawer-2"
             className="btn btn-primary drawer-button lg:hidden"
@@ -16,15 +23,17 @@ const Dashboard = () => {
             Open drawer
           </label>
         </div>
-        <div className="drawer-side">
+        <div className="drawer-side bg-[#D1A054]">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-            <li>
-              <Link><FaShoppingCart />My Cart</Link>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
+          <ul className="menu p-4 w-80 ">
+            <li><NavLink to="/dashboard/home"><FaHome />User Home</NavLink></li>
+            <li><NavLink to="/dashboard/reservations"><FaCalendarAlt/>Reservations</NavLink></li> 
+            <li><NavLink to="/dashboard/history"><FaWallet/>Payment Histroy</NavLink></li>
+            <li><NavLink to="/dashboard/mycart"><FaShoppingCart/>My Cart</NavLink></li>
+            <div className="divider"></div>
+            <li><NavLink to="/"><FaHome />Home</NavLink></li>
+            <li><NavLink to="/menu">Our Menu</NavLink></li>
+            <li><NavLink to="/menu">Order Food</NavLink></li>
           </ul>
         </div>
       </div>
--- bistro-boss-client/src/Pages/DashBoard/MyCart/MyCart.jsx
+++ bistro-boss-client/src/Pages/DashBoard/MyCart/MyCart.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import useCart from '../../../hooks/useCart';
 
 const MyCart = () => {
+    const [cart] = useCart();
+    console.log(cart);
+    const total = cart.reduce((sum,item)=> item.price + sum, 0);
     return (
         <div>
-            <h2>My cart</h2>
+            <h3 className="text-3xl">Total items : {cart.length}</h3>
+            <h3 className="text-3xl">Total price : $ {total}</h3>
+            <button className="btn btn-warning btn-sm">Pay</button>
         </div>
     );
 };
