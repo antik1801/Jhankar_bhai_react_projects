c5b7b34c3f72b02f18971996e6d2e0070291fa5a
antik1801 <antik.edu@gmail.com>
2023-05-30 17:26:53 +0600

    main

--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -34,7 +34,7 @@ async function run() {
                 const user = req.body;
                 const query = { email: user.email }
                 const existingUser = await userCollection.findOne(query)
-                console.log('existing user',existingUser);
+                console.log('existing user', existingUser);
                 if (existingUser) {
                     return res.send({ message: 'User Already Existes' })
                 }
@@ -47,10 +47,10 @@ async function run() {
 
         // Get Method
 
-        app.get('/users', async (req,res) =>{
+        app.get('/users', async (req, res) => {
             try {
                 const result = await userCollection.find().toArray();
-                res.send(result)            
+                res.send(result)
             } catch (error) {
                 res.send(error.message)
             }
@@ -89,6 +89,22 @@ async function run() {
                 res.send(error.message)
             }
 
+        })
+        // patch apis
+        app.patch('users/admin/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const filter = { _id: new ObjectId(id) }
+                const updateDoc = {
+                    $set: {
+                        role: "admin"
+                    },
+                };
+                const result = await userCollection.updateOne(filter,updateDoc)
+                res.send(result)
+            } catch (error) {
+                res.send(error.message)
+            }
         })
 
         //cart collection
