15c1be3b0dc1fd7e8d37bd68bac122cc93833530
antik1801 <antik.edu@gmail.com>
2023-06-01 19:37:40 +0600

    main

--- AIRCNC/client/src/Pages/Dashboard/AddRoom.jsx
+++ AIRCNC/client/src/Pages/Dashboard/AddRoom.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import AddRoomForm from '../../components/Forms/AddRoomForm';
 
 const AddRoom = () => {
+    // handleFormSubmit 
+    const handleFormSubmit = event =>{
+        event.preventDefault();
+        console.log('Hello from form');
+    }
     return (
         <div>
-            <AddRoomForm></AddRoomForm>
+            <AddRoomForm handleFormSubmit={handleFormSubmit}></AddRoomForm>
         </div>
     );
 };
