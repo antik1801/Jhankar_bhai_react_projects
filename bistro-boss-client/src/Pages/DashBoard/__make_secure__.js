2e89d1b19c1cb2a14c24f6fdffcf320a0a93818f
antik1801 <antik.edu@gmail.com>
2023-06-03 15:47:28 +0600

    main

--- bistro-boss-client/src/Pages/DashBoard/__make_secure__.js
+++ bistro-boss-client/src/Pages/DashBoard/__make_secure__.js
@@ -1,6 +1,18 @@
 /**
+ * ---------------------------------------------------
+ *                  BASIC
+ * ---------------------------------------------------
+ * 
  * 1. Donot show the link to them who should not see it
  * 2. only show the person / types of user who should show it
  * 3. do not allow to visit the link by typing on the url
- * 4.
+ * 
+ * ------------------------------------------------------
+ *                  To send data
+ * ------------------------------------------------------
+ * 1. verify JWTtoken(identify admin/user)
+ * 2. If it is an admin activity make sure only admin is posting data
+ * by using verifyAdmin
+ * 
+ * 
  */
