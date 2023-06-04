62ef64e5535c0a79d838db4351759fdd710a2c76
antik1801 <antik.edu@gmail.com>
2023-06-04 15:54:35 +0600

    payment connected

--- bistro-boss-client/src/Pages/DashBoard/Payment/Payment.jsx
+++ bistro-boss-client/src/Pages/DashBoard/Payment/Payment.jsx
@@ -1,11 +1,19 @@
 
+import { loadStripe } from '@stripe/stripe-js';
 import SectionTitle from '../../../components/SectionTitle';
+import CheckoutForm from './CheckoutForm';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripPromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK)
 
 const Payment = () =>{
  return (
 <div className='mt-10'>
     <SectionTitle heading="Payment" subheading="Please process to"></SectionTitle>
     <h2 className="text-3xl">Teka o taka tumi uira uira aso.......</h2>
+    <Elements stripe={stripPromise}>
+    <CheckoutForm />
+    </Elements>
 </div>
  )
 
