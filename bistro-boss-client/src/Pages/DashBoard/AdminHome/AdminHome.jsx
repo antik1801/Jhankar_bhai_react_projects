7778019aa7fa0164187e9ca3f1d73770b9dd8b2f
antik1801 <antik.edu@gmail.com>
2023-06-06 17:18:47 +0600

    final commit

--- bistro-boss-client/src/Pages/DashBoard/AdminHome/AdminHome.jsx
+++ bistro-boss-client/src/Pages/DashBoard/AdminHome/AdminHome.jsx
@@ -3,10 +3,11 @@ import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import AdminDashboardComp from "../../../components/AdminDashboardComp";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid , PieChart, Pie, Sector, ResponsiveContainer, Legend} from "recharts";
 import TriangleBar from "../../../components/TriangleBar";
 
 const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
 const AdminHome = () => {
   const { user } = useAuth();
   const [axiosSecure] = useAxiosSecure();
@@ -40,6 +41,18 @@ const AdminHome = () => {
 
     return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
   };
+  const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
   return (
     <div className="w-full m-4">
       <h4 className="text-3xl">Welcome Back, {user?.displayName}</h4>
@@ -127,7 +140,8 @@ const AdminHome = () => {
           <div className="stat-desc">↘︎ 90 (14%)</div>
         </div>
       </div>
-      <div className="w-full mt-10">
+      <div className="w-full mt-10 flex">
+        <div className="w-1/2">
         <BarChart
           width={500}
           height={300}
@@ -153,6 +167,28 @@ const AdminHome = () => {
             ))}
           </Bar>
         </BarChart>
+        </div>
+        <div className="w-1/2">
+        <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Legend></Legend>
+          <Pie
+            data={chartData}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="count"
+          >
+            {chartData.map((entry, index) => (
+              <Cell name={entry.category} key={`cell-${index}`} fill={colors[index % colors.length]} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+        </div>
       </div>
       <AdminDashboardComp></AdminDashboardComp>
     </div>
