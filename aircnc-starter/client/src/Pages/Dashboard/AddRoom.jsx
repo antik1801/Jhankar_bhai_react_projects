1530263e372d30623ab77c3bfada787a1e3a1bc2
antik1801 <antik.edu@gmail.com>
2023-06-18 00:08:21 +0600

    responsive form

--- aircnc-starter/client/src/Pages/Dashboard/AddRoom.jsx
+++ aircnc-starter/client/src/Pages/Dashboard/AddRoom.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AddRoomForm from '../../components/forms/AddRoomForm';
+import { imageUpload } from '../../utils/imgUpload';
 
 const AddRoom = () => {
     const [loading,setLoading] = useState(false)
@@ -9,8 +10,8 @@ const AddRoom = () => {
         setLoading(true)
         const location = event.target.location.value;
         const title = event.target.title.value;
-        const from = dates.startDate;
-        const to=dates.endDate;
+        // const from = dates.startDate;
+        // const to=dates.endDate;
         const price = event.target.price.value;
         const total_guest = event.target.total_guest.value;
         const bedrooms = event.target.bedrooms.value;
@@ -19,7 +20,12 @@ const AddRoom = () => {
         const category = event.target.category.value;
         const image = event.target.image.files[0];
 
-
+        // upload image
+        imageUpload(image)
+        .then(data=>{
+            console.log(data);
+        })
+        setLoading(false);
     }
     const handleImageChange = image =>{
         setUploadButtonText(image.name)
--- aircnc-starter/client/src/components/forms/AddRoomForm.jsx
+++ aircnc-starter/client/src/components/forms/AddRoomForm.jsx
@@ -73,6 +73,7 @@ const AddRoomForm = ({
                 <div className="flex flex-col w-max mx-auto text-center">
                   <label>
                     <input
+                      onChange={(event)=>handleImageChange(event.target.files[0])}
                       className="text-sm cursor-pointer w-36 hidden"
                       type="file"
                       name="image"
