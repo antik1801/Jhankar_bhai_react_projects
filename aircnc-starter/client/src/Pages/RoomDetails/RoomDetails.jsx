774a232020a12eabf15b4d606dc2ba45f0889c47
antik1801 <antik.edu@gmail.com>
2023-06-17 20:21:03 +0600

    main

--- aircnc-starter/client/src/Pages/RoomDetails/RoomDetails.jsx
+++ aircnc-starter/client/src/Pages/RoomDetails/RoomDetails.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import Container from '../../components/Shared/Container';
-import Header from '../../components/Rooms/Header';
+import React from "react";
+import Container from "../../components/Shared/Container";
+import Header from "../../components/Rooms/Header";
+import RoomInfo from "../../components/Rooms/RoomInfo";
 
 const RoomDetails = () => {
-    return (
-       <Container>
-        <div className='max-w-screen-lg mx-auto'>
-            <div className='flex flex-col gap-6'>
-                <Header></Header>
-                <div>RoomInfo</div>
-                <div>Reservation</div>
-            </div>
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <Header></Header>
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <RoomInfo></RoomInfo>
+            <div>Reservation</div>
+          </div>
         </div>
-       </Container>
-    );
+      </div>
+    </Container>
+  );
 };
 
 export default RoomDetails;
--- aircnc-starter/client/src/routes/Routes.jsx
+++ aircnc-starter/client/src/routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
 import RoomDetails from "../Pages/RoomDetails/RoomDetails";
+import PrivateRoute from "./PrivateRoute";
 
 
 export const router = createBrowserRouter([
@@ -17,7 +18,7 @@ export const router = createBrowserRouter([
       },
       {
         path:"room/:id",
-        element: <RoomDetails></RoomDetails>,
+        element: <PrivateRoute><RoomDetails></RoomDetails></PrivateRoute>,
 
       }
     ],
