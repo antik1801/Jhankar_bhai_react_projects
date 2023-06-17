2e3a8588b38bfdf157efaccb5f3bc7ffcbc3b9dc
antik1801 <antik.edu@gmail.com>
2023-06-17 19:59:51 +0600

    main

--- aircnc-starter/client/src/components/Rooms/Header.jsx
+++ aircnc-starter/client/src/components/Rooms/Header.jsx
@@ -6,8 +6,9 @@ const Header = () => {
         <>
         <Heading title="Veluvana Bali - Owl Bamboo House" subtitle="Khao Lak, Thailand"></Heading>
         <div className='w-full md:h-[60vh] overflow-hidden rounded-xl'>
-            <img src="https://a0.muscache.com/im/pictures/4f70b681-a792-4530-8c52-f2a8d262942d.jpg" alt="" />
+            <img src="https://a0.muscache.com/im/pictures/4f70b681-a792-4530-8c52-f2a8d262942d.jpg" alt="header image" className='object-cover w-full'/>
         </div>
+        
         </>
     );
 };
