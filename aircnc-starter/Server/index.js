9b7972f4f2fe5a0ce0319544bdbee4502191e429
antik1801 <antik.edu@gmail.com>
2023-06-17 22:15:04 +0600

    users collection

--- aircnc-starter/Server/index.js
+++ aircnc-starter/Server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 const corsOptions = {
@@ -12,6 +13,50 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zycuvps.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    const usersCollection = client.db('aircnc2').collection('users')
+
+    // save user email and role in userdb
+    app.put('/users/:email', async(req,res)=>{
+      const email = req.params.email;
+      const user = req.body;
+      const query = {email: email}
+      const options = {
+        upsert: true
+      }
+      const updateDoc = {
+        $set: user
+      }
+      const result = await usersCollection.updateOne(query,updateDoc,options)
+      console.log(result)
+      res.send(result)
+    })
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('AirCNC Server is running..')
