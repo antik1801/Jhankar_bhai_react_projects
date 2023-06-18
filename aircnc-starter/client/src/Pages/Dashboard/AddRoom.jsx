8d40b7bec70e06322f5106cf54495ab46feb42ac
antik1801 <antik.edu@gmail.com>
2023-06-18 09:05:44 +0600

    main

--- aircnc-starter/client/src/Pages/Dashboard/AddRoom.jsx
+++ aircnc-starter/client/src/Pages/Dashboard/AddRoom.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import AddRoomForm from "../../components/forms/AddRoomForm";
 import { imageUpload } from "../../utils/imgUpload";
 import { toast } from "react-hot-toast";
+import useAuth from "../../hooks/useAuth";
 
 const AddRoom = () => {
+  const { user } = useAuth();
   const [loading, setLoading] = useState(false);
   const [uploadButtonText, setUploadButtonText] = useState("Upload Image");
   const handleFormSubmit = (event) => {
@@ -21,23 +23,40 @@ const AddRoom = () => {
     const category = event.target.category.value;
     const image = event.target.image.files[0];
     // upload image
-    const formData = new FormData()
-    formData.append("image",image)
-    const imgbb_api_url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_KEY}`
-    fetch(imgbb_api_url,{
-        method:'POST',
-        body: formData,
+    const formData = new FormData();
+    formData.append("image", image);
+    const imgbb_api_url = `https://api.imgbb.com/1/upload?key=${
+      import.meta.env.VITE_IMGBB_KEY
+    }`;
+    fetch(imgbb_api_url, {
+      method: "POST",
+      body: formData,
     })
-    .then(res=>res.json())
-    .then(imageData=>{
-        setLoading(false)
-        console.log(imageData.data.display_url)
-    })
-    .catch(error=>{
-        setLoading(false)
-        console.log(error.message)
-        toast.error(error.message)
-    })
+      .then((res) => res.json())
+      .then((imageData) => {
+        setLoading(false);
+        const roomData = {
+          location,
+          title,
+          price,
+          total_guest,
+          bedrooms,
+          bathrooms,
+          description,
+          category,
+          image: imageData.data.display_url,
+          host: {
+            name: user?.displayName,
+            image: user?.photoURL,
+            email: user?.email,
+          },
+        };
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error.message);
+        toast.error(error.message);
+      });
     // setLoading(false);
   };
   const handleImageChange = (image) => {
