b27b2d47d10c53a22bfd6ca1d31e69fcf6c13081
antik1801 <antik.edu@gmail.com>
2023-07-02 10:01:02 +0600

    particle js installed

--- Bistro-Boss/client/src/Layouts/Main.jsx
+++ Bistro-Boss/client/src/Layouts/Main.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Container from '../Components/Container';
+import Navbar from '../Pages/Shared/Navbar/Navbar';
+import Particlejs from '../Components/Particlejs';
 
 const Main = () => {
     return (
         <div>
             <Container>
+            <Particlejs></Particlejs>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <div data-aos="fade-left">
             <Footer></Footer>
+            </div>
             </Container>
         </div>
     );
--- Bistro-Boss/client/src/Pages/Shared/Footer/Footer.jsx
+++ Bistro-Boss/client/src/Pages/Shared/Footer/Footer.jsx
@@ -1,11 +1,25 @@
+import moment from "moment";
 import React from "react";
+import Wave from "react-wavify";
+import Particlejs from "../../../Components/Particlejs";
 
 const Footer = () => {
   return (
     <div>
-      <footer className="footer p-10 bg-neutral text-neutral-content">
+      
+      <Wave
+        fill="#2B3440"
+        paused={false}
+        options={{
+          height: 20,
+          amplitude: 40,
+          speed: 0.35,
+          points: 3,
+        }}
+      ></Wave>
+      <footer className="footer p-10 -mt-2  bg-neutral text-neutral-content">
         <div>
-          <img src="/logo.png" alt="" width={100}/>
+          <img src="/logo.png" alt="" width={100} />
           <p>
             Bistro Boss limited
             <br />
@@ -48,10 +62,7 @@ const Footer = () => {
                 <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
               </svg>
             </a>
-            <a
-              href="https://github.com"
-              className="scale-150 mt-1 ml-1"
-            >
+            <a href="https://github.com" className="scale-150 mt-1 ml-1">
               <svg
                 aria-hidden="true"
                 width="24"
@@ -59,22 +70,32 @@ const Footer = () => {
                 viewBox="0 0 24 24"
                 className="fill-current"
               >
-                <path
-                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-                ></path>
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
               </svg>
             </a>
             <a href="https://www.linkedin.com">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+              </svg>
             </a>
           </div>
         </div>
       </footer>
       <footer className="footer footer-center p-4 bg-base-300 text-base-content">
         <div>
-          <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+          <p>
+            Copyright ©{" "}
+            <span className="font-bold">{moment().format("YYYY/MM/DD")}</span> -
+            All right reserved by Bistro Boss
+          </p>
         </div>
       </footer>
+      {/* <Particlejs></Particlejs> */}
     </div>
   );
 };
