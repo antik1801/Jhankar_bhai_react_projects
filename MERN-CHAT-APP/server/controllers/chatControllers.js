c395e115481543a78200dd5e22262803d7945191
antik1801 <antik.edu@gmail.com>
2023-07-17 04:33:12 +0600

    all api's endpoint added

--- MERN-CHAT-APP/server/controllers/chatControllers.js
+++ MERN-CHAT-APP/server/controllers/chatControllers.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler');
 const Chat = require('../models/chatModal');
 const User = require('../models/userModel');
 
-const accessChat = asyncHandler( async (req,res)=>{
+const accessChat = asyncHandler(async (req, res) => {
     const { userId } = req.body;
 
     if (!userId) {
@@ -12,29 +12,29 @@ const accessChat = asyncHandler( async (req,res)=>{
 
     var isChat = await Chat.find({
         isGroupChat: false,
-        $and:[
-            {users : {$elemMatch: {$eq: req.user._id}}},
-            {users : {$elemMatch: {$eq: userId}}},
+        $and: [
+            { users: { $elemMatch: { $eq: req.user._id } } },
+            { users: { $elemMatch: { $eq: userId } } },
         ],
-    }).populate('users',"-password").populate("latestMessage")
+    }).populate('users', "-password").populate("latestMessage")
     isChat = await User.populate(isChat, {
         path: 'latestMessage.sender',
         select: "name pic email"
     })
 
-    if (isChat.length>0) {
+    if (isChat.length > 0) {
         res.send(isChat[0]);
     }
     else {
         var chatData = {
-            chatName : "sender",
+            chatName: "sender",
             isGroupChat: false,
-            users: [ req.user._id, userId ]
+            users: [req.user._id, userId]
         };
 
         try {
             const createdChat = await Chat.create(chatData)
-            const FullChat = await Chat.findOne({_id:createdChat._id}).populate('users',"-password")
+            const FullChat = await Chat.findOne({ _id: createdChat._id }).populate('users', "-password")
             res.status(200).send(FullChat)
         } catch (error) {
             console.log(error)
@@ -44,20 +44,20 @@ const accessChat = asyncHandler( async (req,res)=>{
     }
 })
 
-const fetchChat = asyncHandler(async(req,res)=>{
+const fetchChat = asyncHandler(async (req, res) => {
     try {
-        Chat.find({users: {$elemMatch: {$eq: req.user._id}}})
-        .populate("users", "-password")
-        .populate("groupAdmin", "-password")
-        .populate("latestMessage")
-        .sort({updatedAt: -1})
-        .then(async(results)=>{
-            results = await User.populate(results, {
-                path: 'latestMessage.sender',
-                select: "name pic email"
-            });
-            res.status(200).send(results)
-        })
+        Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+            .populate("latestMessage")
+            .sort({ updatedAt: -1 })
+            .then(async (results) => {
+                results = await User.populate(results, {
+                    path: 'latestMessage.sender',
+                    select: "name pic email"
+                });
+                res.status(200).send(results)
+            })
     } catch (error) {
         console.log(error)
         res.status(400)
@@ -65,9 +65,9 @@ const fetchChat = asyncHandler(async(req,res)=>{
     }
 })
 
-const createGroupChat = asyncHandler(async(req,res)=>{
+const createGroupChat = asyncHandler(async (req, res) => {
     if (!req.body.users || !req.body.name) {
-        return res.status(400).send({message: "Please Fill all the fields"})
+        return res.status(400).send({ message: "Please Fill all the fields" })
     }
     var users = JSON.parse(req.body.users)
     if (users.length < 2) {
@@ -81,9 +81,9 @@ const createGroupChat = asyncHandler(async(req,res)=>{
             isGroupChat: true,
             groupAdmin: req.user,
         })
-        const fullGroupChat = await Chat.findOne({_id: groupChat._id})
-        .populate("users", "-password")
-        .populate("groupAdmin", "groupAdmin");
+        const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+            .populate("users", "-password")
+            .populate("groupAdmin", "groupAdmin");
 
         res.status(200).json(fullGroupChat)
     } catch (error) {
@@ -92,7 +92,7 @@ const createGroupChat = asyncHandler(async(req,res)=>{
     }
 })
 
-const renameGroup = asyncHandler(async(req,res)=>{
+const renameGroup = asyncHandler(async (req, res) => {
     const { chatId, chatName } = req.body
     const updatedChat = await Chat.findByIdAndUpdate(
         chatId,
@@ -103,16 +103,54 @@ const renameGroup = asyncHandler(async(req,res)=>{
             new: true,
         }
     )
-    .populate("users", "-password")
-    .populate("groupAdmin", "-password")
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password")
 
     if (!updatedChat) {
         res.status(404);
         throw new Error("Chat Not Found")
     }
-    else{
+    else {
         res.json(updatedChat)
     }
 })
 
-module.exports = {accessChat, fetchChat, createGroupChat, renameGroup}
+const addToGroup = asyncHandler(async (req, res) => {
+    const { chatId, userId } = req.body
+    const added = await Chat.findByIdAndUpdate(chatId, {
+        $push: { users: userId },
+    },
+        { new: true },
+    )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+    if (!added) {
+        res.status(404)
+        throw new Error("Chat Not Found")
+    }
+    else{
+        res.json(added)
+    }
+})
+
+const removeFromGroup = asyncHandler(async(req,res)=>{
+    const { chatId, userId } = req.body
+    const remove = await Chat.findByIdAndUpdate(
+        chatId, 
+        {
+        $pull: { users: userId },
+        },
+        { new: true },
+    )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+    if (!remove) {
+        res.status(404)
+        throw new Error("Chat Not Found")
+    }
+    else{
+        res.json(remove)
+    }
+})
+
+module.exports = { accessChat, fetchChat, createGroupChat, renameGroup, addToGroup, removeFromGroup }
--- MERN-CHAT-APP/server/routes/chatRoutes.js
+++ MERN-CHAT-APP/server/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
-const { accessChat, fetchChat, createGroupChat, renameGroup } = require('../controllers/chatControllers');
+const { accessChat, fetchChat, createGroupChat, renameGroup, addToGroup, removeFromGroup } = require('../controllers/chatControllers');
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.route('/').post(protect, accessChat)
 router.route('/').get(protect, fetchChat)
 router.route('/group').post(protect, createGroupChat)
 router.route('/rename').put(protect, renameGroup)
-// router.route('/groupremove').put(protect, removeFromGroup)
-// router.route('/groupadd').put(protect, addToGroup) 
+router.route('/groupremove').put(protect, removeFromGroup)
+router.route('/groupadd').put(protect, addToGroup) 
 
 module.exports = router
