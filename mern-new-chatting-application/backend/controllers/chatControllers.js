a7858db74e0f5ca073a05b40fd1022db0196c39d
antik1801 <antik.edu@gmail.com>
2023-07-27 13:38:22 +0600

    added

--- mern-new-chatting-application/backend/controllers/chatControllers.js
+++ mern-new-chatting-application/backend/controllers/chatControllers.js
@@ -46,7 +46,7 @@ const accessChat = asyncHandler(async(req,res)=>{
 
 const fetchChats = asyncHandler(async(req,res)=>{
     try {
-        
+        Chat.find({users: {$elemMatch: {$eq: req.user._id}}}).then(result=>res.send(result))
     } catch (error) {
         
     }
