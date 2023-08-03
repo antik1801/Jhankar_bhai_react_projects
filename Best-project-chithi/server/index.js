51541171ce05aad84b071f6d0f5896f264d32054
antik1801 <antik.edu@gmail.com>
2023-08-03 14:07:51 +0600

    main

--- Best-project-chithi/server/index.js
+++ Best-project-chithi/server/index.js
@@ -8,21 +8,25 @@ import multer from "multer"
 import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
-import { fileURLToPath } from "url"
+import { MongoClient, ServerApiVersion } from "mongodb"
+
 
 // configarations /
-
-const __filename = fileURLToPath(import.meta.url)   // used to save file in malter
-const __dirname = path.dirname(__filename)  // pathname adde file name save
 dotenv.config() 
 
 const app = express()
 app.use(express.json())   // req json file 
 app.use(helmet()) // henadler of error
 app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"})) // cross origin errors
-
 app.use(morgan("common"))
 app.use(bodyParser.json({limit: "30mb" , extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+// Mongoose setup
+const PORT = process.env.PORT
+
+
 
 
 
