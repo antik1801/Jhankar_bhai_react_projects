437863df5cbd79f83676c6f0e4fd2d7afd8cd536
antik1801 <antik.edu@gmail.com>
2023-08-04 05:07:31 +0600

    serverjs

--- chithi-project/index.js
+++ chithi-project/index.js
@@ -9,4 +9,12 @@ app.get("/", (req,res)=>{
     res.send("API is running")
 })
 
+app.get('/api/chat', (req,res)=>{
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', (req,res)=>{
+    console.log(req)
+})
+
 app.listen(5000, console.log("Server started on PORT 5000"))
