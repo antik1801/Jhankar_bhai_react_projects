6596738e0325a2bcb7522eb90f902253a32e6b8a
antik1801 <antik.edu@gmail.com>
2023-08-04 04:49:08 +0600

    backend 1

---
+++ chithi-project/backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const chats  = require("./data/data")
+const app = express()
+require('dotenv').config()
+
+
+
+app.get("/", (req,res)=>{
+    res.send("API is running")
+})
+
+app.get('/api/chat', (req,res)=>{
+    res.send(chats)
+})
+
+app.listen(5000, console.log("Server started on PORT 5000"))
