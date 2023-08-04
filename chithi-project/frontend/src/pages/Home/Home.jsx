a77504a8662c963fde0b6ec4f17660b203cad807
antik1801 <antik.edu@gmail.com>
2023-08-04 17:10:23 +0600

    main

--- chithi-project/frontend/src/pages/Home/Home.jsx
+++ chithi-project/frontend/src/pages/Home/Home.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
-import "./Home.css"
-import { Box, Container, Text } from "@chakra-ui/react"
+import React from "react";
+import "./Home.css";
+import { Box, Container, Text } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 // Home is the app.jsx
 const Home = () => {
-    return (
-       <Container maxW='xl' centerContent>
-        <Box 
-        display='flex' justifyContent='center' p={3}
-        bg={'white'} w="100%" m="40px 0 15px 0" 
-        borderRadius="lg" borderWidth="1px"
-        >
-            <Text fontSize="4xl" fontFamily="Work sans">Chithi - APP</Text>
-        </Box>
-        <Box bg="white" w={"100%"} p="4" borderRadius="lg" borderWidth="1px"></Box>
-
-       </Container>
-    );
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Chithi - APP
+        </Text>
+      </Box>
+      <Box bg="white" w={"100%"} p="4" borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded" colorScheme="orange">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">SignUp</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              {/* <Login></Login> */}
+            </TabPanel>
+            <TabPanel>
+              {/* <SignUp></SignUp> */}
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default Home;
