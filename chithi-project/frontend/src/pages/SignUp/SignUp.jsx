aadf3b74c2193a66f7c2502a7d5eb824a7ebf5ad
antik1801 <antik.edu@gmail.com>
2023-08-05 12:52:23 +0600

    frontend part added

--- chithi-project/frontend/src/pages/SignUp/SignUp.jsx
+++ chithi-project/frontend/src/pages/SignUp/SignUp.jsx
@@ -1,61 +1,147 @@
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useToast } from "@chakra-ui/react";
 
 const SignUp = () => {
-    const [name,setName] = useState("")
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-    const [show,setShow] = useState(false)
-    const [confirmPassword, setConfirmPassword] = useState("")
-    const [pic,setPic] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [pic, setPic] = useState("");
+  const [picLoading, setpicLoading] = useState(false);
+  const toast = useToast();
 
-    const handleClick = () => setShow(!show)
+  const handleClick = () => setShow(!show);
 
-    const postDetails = (pic) =>{
+  const postDetails = (pic) => {
+    setpicLoading(true);
 
+    if (pic === undefined) {
+      toast({
+        title: "Please select an image.",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
     }
 
-    const submitHandler =()=>{}
+    if (
+      pic.type === "image/jpeg" ||
+      pic.type === "image/png" ||
+      pic.type === "image/jpg"
+    ) {
+      const formData = new FormData();
+      formData.append("image", pic);
+      const imgbb_api_url = `https://api.imgbb.com/1/upload?key=${
+        import.meta.env.VITE_IMGBB_KEY
+      }`;
+      fetch(imgbb_api_url, {
+        method: "POST",
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then((imageData) => {
+          const imageUrl = imageData.data.display_url;
+          setPic(imageUrl);
+          setpicLoading(false);
+        })
+        .catch((error) => {
+          setpicLoading(false);
+        });
+    }
+    else 
+    {
+        toast({
+            title: "Only JPEG/JPG/PNG accepted",
+            status: "warning",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom",
+          });
+          return;
+    }
+  };
 
-    return (
-        <VStack spacing='5px'>
-            <FormControl id='first-name' isRequired>
-                <FormLabel>Name</FormLabel>
-                <Input placeholder='Enter Your Name' onChange={(e)=>setName(e.target.value)}></Input>
-            </FormControl>
-            <FormControl id='email' isRequired>
-                <FormLabel>Email</FormLabel>
-                <Input type='email' placeholder='Enter Your Email' onChange={(e)=>setEmail(e.target.value)}></Input>
-            </FormControl>
-            <FormControl id='password' isRequired>
-                <FormLabel>Password</FormLabel>
-                <InputGroup>
-                <Input type={ show ? "text" : "password" } placeholder='Enter Your Password' onChange={(e)=>setPassword(e.target.value)}></Input>
-                <InputRightElement width="4.5rem">
-                    <Button h="1.75rem" size="sm" onClick={handleClick}>
-                        { show ? "Hide" : "Show" }
-                    </Button>
-                </InputRightElement>
-                </InputGroup>
-            </FormControl>
-            <FormControl id='confirm-password' isRequired>
-                <FormLabel>Confirm Password</FormLabel>
-                <InputGroup>
-                <Input type={ show ? "text" : "password" } placeholder='Enter Your Password Again' onChange={(e)=>setConfirmPassword(e.target.value)}></Input>
-                <InputRightElement width="4.5rem">
-                    <Button h="1.75rem" size="sm" onClick={handleClick}>
-                        { show ? "Hide" : "Show" }
-                    </Button>
-                </InputRightElement>
-                </InputGroup>
-            </FormControl>
-            <FormControl id='pic'>
-                <FormLabel>Upload Your Picture</FormLabel>
-                <Input type='file' p={1.5} accept='image/*' onChange={(e)=> postDetails(e.target.files[0])}></Input>
-            </FormControl>
-            <Button colorScheme='purple' width="100%" style={{marginTop: 15}} onClick={submitHandler}>Sign Up</Button>
-        </VStack>
-    );
+  const submitHandler = () => {};
+
+  return (
+    <VStack spacing="5px">
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input
+          placeholder="Enter Your Name"
+          onChange={(e) => setName(e.target.value)}
+        ></Input>
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          type="email"
+          placeholder="Enter Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        ></Input>
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Your Password"
+            onChange={(e) => setPassword(e.target.value)}
+          ></Input>
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="confirm-password" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Your Password Again"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Input>
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="pic">
+        <FormLabel>Upload Your Picture</FormLabel>
+        <Input
+          type="file"
+          p={1.5}
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
+        ></Input>
+      </FormControl>
+      <Button
+        colorScheme="purple"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        isLoading={picLoading}
+      >
+        Sign Up
+      </Button>
+    </VStack>
+  );
 };
 
 export default SignUp;
